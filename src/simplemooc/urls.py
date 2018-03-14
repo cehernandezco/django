5338b47d16f440cf6296820d4c216a5fb8744f95
cehernandezco <cehernandezco@gmail.com>
2018-03-14 18:08:17 +1100

    basic home and contact pages were created. No heritage views yet

--- src/simplemooc/urls.py
+++ src/simplemooc/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from simplemooc.core import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
+
+
 ]
